9233a51a8f651ee6301177ece9c5be4685956ddb
khanglb2000 <lambaokhang2000@gmail.com>
2023-03-15 13:03:08 +0700

    TS basic variable types

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let myName = 'Ken';
+let meaningOfLife;
+let isLoading;
+let album;
+let union;
+myName = 'Khang';
+meaningOfLife = 77;
+isLoading = true;
+album = { type: 2, name: 'Hi' };
+union = 'Oh';
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let reg = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+let myName: string = 'Ken'
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+let union: string | number;
+
+myName = 'Khang'
+meaningOfLife = 77
+isLoading = true
+album = {type: 2, name: 'Hi'}
+union = 'Oh'
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+let postId: string | number;
+let isActive: number | boolean | string;
+
+let reg: RegExp = /\w+/g
+
+
+
